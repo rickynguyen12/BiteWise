e5321e66d219d9b2faee136415227a918d17bccb
BhagyeshRathi <Bhageyesh2161@gmail.com>
2024-03-18 19:12:20 -0700

    removed comments

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const request = require('request');
 const https = require('https');
 
-// const hostname = '127.0.0.1';
-// const port = 3000;
 const app = express();
 
 app.use(express.static("public"))
