27b6880cafd73858ded39703a0c4ee339005805f
rliao123 <92598518+rliao123@users.noreply.github.com>
2024-04-07 20:21:27 -0700

    Updates for Restaurant Page

--- client/src/pages/FoodItemPage.js
+++ client/src/pages/FoodItemPage.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import Footer from "../components/Footer";
 import FrameComponent4 from "../components/FrameComponent4";
 import "./FoodItemPage.css";
+import { TextField, Button } from "@mui/material";
 
 const FoodItemPage = () => {
   const davidAndEmilysPatisserie = {
@@ -12,19 +13,98 @@ const FoodItemPage = () => {
     cost: "$$",
     menu: [
       {
-        id: 1,
-        name: "Plain Croissant",
-        description: "Freshly baked croissant",
-        price: "$5.00",
+        name: "Pastries",
+        items: [
+          {
+            id: 1,
+            name: "Plain Croissant",
+            description:
+              "Freshly baked croissant made from flaky layers of buttery dough",
+            price: "$5.00",
+          },
+          {
+            id: 2,
+            name: "Pain au Chocolat",
+            description: "Buttery pastry with chocolate filling",
+            price: "$5.50",
+          },
+          {
+            id: 3,
+            name: "Ham & Cheese Pastry",
+            description: "Flaky pastry filled with ham and cheese.",
+            price: "$5.00",
+          },
+          {
+            id: 4,
+            name: "Cream Cheese Pastry",
+            description:
+              "Pastry filled with cream cheese and topped with sliced almonds.",
+            price: "$4.50",
+          },
+        ],
       },
       {
-        id: 2,
-        name: "Pain au Chocolat",
-        description: "Buttery pastry with chocolate filling",
-        price: "$5.50",
+        name: "Cookies",
+        items: [
+          {
+            id: 1,
+            name: "Chocolate Chip Cookie",
+            description: "The classic, loaded with chocolate chips.",
+            price: "$2.00",
+          },
+          {
+            id: 2,
+            name: "Double Chocolate Cookie",
+            description: "Chocolate cookie with chocolate chips.",
+            price: "$2.50",
+          },
+          {
+            id: 3,
+            name: "Oatmeal Raisin Cookie",
+            description:
+              "Traditional flavors of cinnamon, oatmeal and sweet raisins.",
+            price: "$2.00",
+          },
+        ],
+      },
+      {
+        name: "Cake",
+        items: [
+          {
+            id: 1,
+            name: "Mixed Berry Cake",
+            description:
+              "Layers of vanilla cake, with cream, strawberries, blueberries, and raspberries.",
+            price: "$34.00",
+          },
+          {
+            id: 2,
+            name: "Chocolate Cake",
+            description:
+              "3 Layer Chocolate Cake, Chocolate Buttercream Filling.",
+            price: "$30.00",
+          },
+          {
+            id: 3,
+            name: "Chocolate Strawberry Cake",
+            description:
+              "3 Layer Chocolate Cake, Whipped Cream Filling with Strawberry.",
+            price: "$32.00",
+          },
+        ],
       },
     ],
   };
+
+  const [selectedCategory, setSelectedCategory] = useState("Pastries");
+
+  const [selectedItems, setSelectedItems] = useState([]);
+
+  const handleAddToCart = (item) => {
+    setSelectedItems([...selectedItems, item]);
+  };
+
+  const handlePlaceOrder = () => {};
 
   return (
     <div className="food-item">
@@ -55,18 +135,87 @@ const FoodItemPage = () => {
                 <p>{davidAndEmilysPatisserie.cost}</p>
               </div>
             </div>
+            <div className="frame-wrapper">
+              <div className="frame-parent5">
+                <TextField
+                  className="frame-textfield"
+                  placeholder="Search for other food items"
+                  variant="outlined"
+                  sx={{
+                    "& fieldset": { borderColor: "#808080" },
+                    "& .MuiInputBase-root": {
+                      height: "49px",
+                      backgroundColor: "#fff",
+                      borderRadius: "10px",
+                      fontSize: "14px",
+                    },
+                    "& .MuiInputBase-input": { color: "#808080" },
+                  }}
+                />
+                <Button
+                  className="sign-in5"
+                  disableElevation={true}
+                  variant="contained"
+                  sx={{
+                    textTransform: "none",
+                    color: "#000",
+                    fontSize: "14",
+                    background: "#fff",
+                    borderRadius: "10px",
+                    "&:hover": { background: "#29a679" },
+                    width: 129,
+                    height: 49,
+                  }}
+                >
+                  Favorite
+                </Button>
+              </div>
+            </div>
           </div>
         </div>
       </section>
       <div className="menu">
-        <div className="menu-items">
-          {davidAndEmilysPatisserie.menu.map((item) => (
-            <div className="menu-item" key={item.id}>
-              <h3>{item.name}</h3>
-              <p>{item.description}</p>
-              <p>{item.price}</p>
+        <div className="menu-category">
+          {davidAndEmilysPatisserie.menu.map((category) => (
+            <div
+              key={category.name}
+              className={`category ${
+                selectedCategory === category.name ? "selected" : ""
+              }`}
+              onClick={() => setSelectedCategory(category.name)}
+            >
+              {category.name}
             </div>
           ))}
+        </div>
+        <div className="menu-items">
+          {selectedCategory &&
+            davidAndEmilysPatisserie.menu
+              .find((category) => category.name === selectedCategory)
+              .items.map((item) => (
+                <div className="menu-item" key={item.id}>
+                  <div className="item-info">
+                    <h3>{item.name}</h3>
+                    <p>{item.description}</p>
+                    <p>{item.price}</p>
+                  </div>
+                  <button class="add-button">Add +</button>
+                </div>
+              ))}
+        </div>
+        <div className="cart">
+          <h2>Cart</h2>
+          <p> from David and Emily's Patisserie</p>
+          <div className="cart-items">
+            {selectedItems.map((item, index) => (
+              <div key={index} className="cart-item">
+                {item.name} - {item.price}
+              </div>
+            ))}
+          </div>
+          <button onClick={handlePlaceOrder} className="place-order">
+            Go To Place Order
+          </button>
         </div>
       </div>
       <Footer propHeight="20.9px" propHeight1="24px" />
