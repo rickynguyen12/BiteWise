6d73b6c63af12ba695ac88c2e492b11e445f6b2d
BhagyeshRathi <Bhageyesh2161@gmail.com>
2024-04-10 11:03:32 -0700

    minor changes

--- server/routes/user.js
+++ server/routes/user.js
@@ -4,11 +4,13 @@ const router = express.Router();
 
 // import controller
 import {register} from '../controllers/user.js';
+import {homepage} from '../controllers/user.js';
 
 // import middlewares
 import {userRegisterValidator} from '../middlewares/user.js';
 
 // api routes
+router.get('/', homepage);
 router.post('/register', userRegisterValidator, register);
 
 export default router;
