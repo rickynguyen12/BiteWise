5535374d5f78add6cd4fb07d85673d66b1a8b75e
BhagyeshRathi <Bhageyesh2161@gmail.com>
2024-04-10 22:23:36 -0700

    added login/logout functionality

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,4 +1,7 @@
 import {User} from '../models/user.js';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
 
 const register = async (req, res) => {
   
@@ -33,5 +36,58 @@ const homepage = async (req, res) => {
   })
 };
 
+const login = async (req, res) => {
+  // find the user based on email
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    // if no user found 
+    if (!user) {
+      return res.status(400).json({
+        error: "Invalid Credentials",
+      });
+    }
+
+    // if user is found, authenticate
+    if (!user.authenticate(password)) {
+      return res.status(400).json({
+        error: "Invalid email or password",
+      });
+    }
+
+    // generate a token with user id and jwt secret
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: '24h' });
+
+    // persist the token as 'jwt' in cookie with an expiry date
+    res.cookie('jwt', token, { expire: new Date() + 9999, httpOnly: true });
+
+    // return the response with user
+    const { username } = user;
+    return res.json({
+      message: "Login Successfull",
+      username,
+    });
+  } catch (err) {
+    console.error("Error during login:", err);
+    return res.status(500).json({
+      error: "Internal Server Error",
+    });
+  }
+};
+
+const logout = async (req, res) => {
+  // clear the cookie
+  res.clearCookie('jwt');
+
+  return res.json({
+    message: "Logout Successfull",
+  });
+};
+
+
 export {register};
 export {homepage};
+export {login};
+export {logout};
--- server/routes/user.js
+++ server/routes/user.js
@@ -5,6 +5,8 @@ const router = express.Router();
 // import controller
 import {register} from '../controllers/user.js';
 import {homepage} from '../controllers/user.js';
+import {login} from '../controllers/user.js';
+import {logout} from '../controllers/user.js';
 
 // import middlewares
 import {userRegisterValidator} from '../middlewares/user.js';
@@ -12,5 +14,7 @@ import {userRegisterValidator} from '../middlewares/user.js';
 // api routes
 router.get('/', homepage);
 router.post('/register', userRegisterValidator, register);
+router.post('/login', login);
+router.get('/logout', logout);
 
 export default router;
