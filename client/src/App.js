a9feee67d99d27823910988e54f8bd193f429543
jananipandu <janani.pandurangan@sjsu.edu>
2024-04-17 19:01:11 -0700

    Fixed Phone Num + Added In App Route

--- client/src/App.js
+++ client/src/App.js
@@ -11,13 +11,11 @@ import Register from "./pages/Register";
 import RegisterAsStoreOwner from "./pages/RegisterAsStoreOwner";
 import FoodItemPage from "./pages/FoodItemPage";
 import FoodItemPage2 from "./pages/FoodItemPage2";
-<<<<<<< HEAD
 import RedirectPageToFoodDelivery from "./pages/RedirectPageToFoodDelivery";
-=======
 import OwnerDashboard from "./pages/OwnerDashboard";
 import OwnerEditProfile from "./pages/OwnerEditProfile";
 import OwnerEditMenu from "./pages/OwnerEditMenu";
->>>>>>> 887bbc7fe78c57659b5f92727667a619ea65b0ca
+import IACustomerCheckout from "./pages/IACustomerCheckout";
 
 function App() {
   const action = useNavigationType();
@@ -85,6 +83,7 @@ function App() {
       <Route path="/owner-dashboard" element={<OwnerDashboard />} />
       <Route path="/owner-edit-profile" element={<OwnerEditProfile />} />
       <Route path="/owner-edit-menu" element={<OwnerEditMenu />} />
+      <Route path="/in-app-checkout" element={<IACustomerCheckout />} />
     </Routes>
   );
 }
