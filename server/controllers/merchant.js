07e19b98193ad906dc6cb3093329fca9a389f604
Jason Tobin <94418504+JasonTobin@users.noreply.github.com>
2024-04-30 14:46:34 -0700

    Edits to Merchant Controller

--- server/controllers/merchant.js
+++ server/controllers/merchant.js
@@ -1,14 +1,10 @@
-import {Merchant} from '../models/user.js';
+import {Merchant} from '../models/merchant.js';
+import jwt from 'jsonwebtoken';
 
 const register = async (req, res) => {
-  
-  // check if username already exists
-  const userNameExists = await Merchant.findOne({
-    username: req.body.username
-  });
-  if (userNameExists) return res.status(403).json({
-    error: "Username already exists"
-  });
+
+  // Business Name, Adress could be duplicates
+  // Email, and business phone number must be individual
 
   // check if email already exists
   const emailExists = await Merchant.findOne({
@@ -18,13 +14,77 @@ const register = async (req, res) => {
     error: "Email already exists"
   });
 
+  const businessPhoneExists = await Merchant.findOne({
+    phone: req.body.businessPhone
+  })
+  if(businessPhoneExists) return res.status(403).json({
+    error: "Business Phone Number Already Exists"
+  })
+
   // create new user if username and email are unique
   const merchant = new Merchant(req.body);
   await merchant.save();
   res.status(201).json({
-    message: "Signup Successfull, Please Login to continue."
+    message: "Signup Successfull, Please Login to continue.",
+    redirect: "/login"
   });
+};
 
+const homepage = async (req, res) => {
+  res.status(200).json({
+    message:"Successful!"
+  })
+};
+
+const login = async (req, res) => {
+  // find the user based on email
+  const { email, password } = req.body;
+
+  try {
+    const merchant = await Merchant.findOne({ email });
+
+    // if no user found 
+    if (!merchant) {
+      return res.status(400).json({
+        error: "Invalid Credentials",
+      });
+    }
+
+    // if user is found, authenticate
+    if (!merchant.authenticate(password)) {
+      return res.status(400).json({
+        error: "Invalid email or password",
+      });
+    }
+
+    // generate a token with user id and jwt secret
+    const token = jwt.sign({ _id: merchant._id }, process.env.JWT_SECRET, { expiresIn: '24h' });
+
+    // persist the token as 'jwt' in cookie with an expiry date
+    res.cookie('jwt', token, { expire: new Date() + 9999, httpOnly: true });
+
+    // return the response with user
+    const { email } = merchant;
+    return res.json({
+      message: "Login Successfull",
+      email,
+    });
+  } catch (err) {
+    console.error("Error during login:", err);
+    return res.status(500).json({
+      error: "Internal Server Error",
+    });
+  }
+};
+
+const logout = async (req, res) => {
+  res.clearCookie('jwt');
+  res.json({
+    message: "Logout Successfull"
+  });
 };
 
 export {register};
+export {homepage};
+export {login};
+export {logout};
