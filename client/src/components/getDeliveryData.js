1fbaa580551b0831848e78b1ea4a0c12091bd5db
rliao123 <92598518+rliao123@users.noreply.github.com>
2024-05-11 15:18:02 -0700

    update

--- client/src/components/getDeliveryData.js
+++ client/src/components/getDeliveryData.js
@@ -2,15 +2,15 @@ export function generateUniqueArray(number) {
   const array = [];
 
   // Generate unique random floats and add them to the array
-  var i = 4
-  var j = 0
-  var addnum = 4.25
+  var i = 4;
+  var j = 0;
+  var addnum = 1.5;
   while (j < 4) {
-    array.push((number % i) + addnum)
-    number += 1
-    j++
-    addnum += 0.25
+    array.push((number % i) + addnum);
+    number += 1;
+    j++;
+    addnum += 0.25;
   }
-  
+
   return array;
 }
