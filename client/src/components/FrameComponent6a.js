fcf75709c0e0b42298b357a0158e071ec0ffbb64
ardwaino <mr.shreyass@gmail.com>
2024-05-12 20:33:11 -0700

    reverted framcomponent6a.css->6.css

--- client/src/components/FrameComponent6a.js
+++ client/src/components/FrameComponent6a.js
@@ -1,6 +1,6 @@
 import { Button } from "@mui/material";
 import { Link } from "react-router-dom";
-import "./FrameComponent6a.css";
+import "./FrameComponent6.css";
 import { useNavigate } from "react-router-dom";
 import "@lottiefiles/lottie-player";
 
